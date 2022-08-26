c607208bbc42bdacdcca4a71a6adfd20ad4bf39d
sahib2442 <kadersahib1234@gmail.com>
2022-08-26 15:36:16 +0530

    [sahib] Add: swap two number

---
+++ SwapTwoNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace SwapTwoNumber
+{
+    class program
+    {
+        static void main(string[]args)
+        {
+            SwapTwoNumber a = new SwapTwoNumber;
+            a.SwapTwoNumber;
+            Console.WriteLine(SwapTwoNumber);
+        }
+    }
+}
